6b51923a37a944e815e20c1ff6a0c914d855761c
RepositorioLuismendoza <luis.mendoza@udea.edu.co>
2023-04-19 15:07:39 +0100

    arreglemos una parte del codigo que extrae la informacion

--- parcial_1/funciones.cpp
+++ parcial_1/funciones.cpp
@@ -31,34 +31,34 @@ char* leer()
 
 
 
-char* informacion(char* cadena,char* buscando)
+char* informacion(char* cadena, char* buscando)
 {
-    char* inicio = cadena; // puntero que apunta al inicio de la cadena
-    char *codigo=nullptr; // cambiamos los arreglos a punteros y los ponm0s nulos al principio
-    char *nombre=nullptr; // cambiamos los arreglos a punteros
-    char *creditos=nullptr; // cambiamos los arreglos a punteros
-    char *horas=nullptr; // cambiamos los arreglos a punteros
-    char* resultado = new char[100]; // arreglo donde almacenaremos la información encontrada
-    int posicionResultado = 0; // variable que nos indica la posición actual en el arreglo resultado
+    char* inicio = cadena;
+    char* codigo = nullptr;
+    char* nombre = nullptr;
+    char* creditos = nullptr;
+    char* horas = nullptr;
+    char* resultado = new char[100];
+    int posicionResultado = 0;
+    bool encontrado = false;
 
-    while(*inicio != '\0') {
-        // sacamos el código
+    while (*inicio != '\0') {
+        // Sacamos el código
         int i = 0;
-        codigo=new char[10];//le ponemos dinamica en esa vaina aver que sale
-        while(*inicio != ' ') {
+        codigo = new char[10];
+        while (*inicio != ' ') {
             codigo[i] = *inicio;
             i++;
             inicio++;
         }
+        codigo[i] = '\0';
 
-        codigo[i] = '\0'; // verificamos que termine con el carácter nulo
+        inicio++;
 
-        inicio++; // saltamos el espacio en blanco
-
-        // sacar  el nombre
+        // Sacar el nombre
         i = 0;
-        nombre=new char[40];//cuarenta por que las materias tienen nombres largos
-        while(*inicio != ' ') {
+        nombre = new char[40];
+        while (*inicio != ' ') {
             nombre[i] = *inicio;
             i++;
             inicio++;
@@ -66,10 +66,10 @@ char* informacion(char* cadena,char* buscando)
         nombre[i] = '\0';
         inicio++;
 
-        // sacar los creditos
+        // Sacar los créditos
         i = 0;
-        creditos=new char[2];//los creditos son pequeños
-        while(*inicio != ' ') {
+        creditos = new char[2];
+        while (*inicio != ' ') {
             creditos[i] = *inicio;
             i++;
             inicio++;
@@ -77,10 +77,10 @@ char* informacion(char* cadena,char* buscando)
         creditos[i] = '\0';
         inicio++;
 
-        // sacar las horas
+        // Sacar las horas
         i = 0;
-        horas=new char[2];
-        while(*inicio != '\r') {
+        horas = new char[2];
+        while (*inicio != '\r') {
             horas[i] = *inicio;
             i++;
             inicio++;
@@ -88,74 +88,59 @@ char* informacion(char* cadena,char* buscando)
         horas[i] = '\0';
         inicio++;
 
-        //vamos a ver si es igual al buscado
-        if (es_igual(codigo, buscando))
-        {
-            cout<<"si esta"<<endl;
-            // almacenamos la información encontrada en el arreglo resultado
-            for(int j = 0; codigo[j] != '\0'; j++)
-            {
+        // Verificar si es igual al buscado
+        if (es_igual(codigo, buscando)) {
+            encontrado = true;
+            // Almacenamos la información encontrada en el arreglo resultado
+            for (int j = 0; codigo[j] != '\0'; j++) {
                 resultado[posicionResultado] = codigo[j];
                 posicionResultado++;
             }
-                resultado[posicionResultado] = ' ';
-                posicionResultado++;
+            resultado[posicionResultado] = ' ';
+            posicionResultado++;
 
-            for(int j = 0; nombre[j] != '\0'; j++)
-            {
+            for (int j = 0; nombre[j] != '\0'; j++) {
                 resultado[posicionResultado] = nombre[j];
                 posicionResultado++;
             }
-                resultado[posicionResultado] = ' ';
-                posicionResultado++;
+            resultado[posicionResultado] = ' ';
+            posicionResultado++;
 
-            for(int j = 0; creditos[j] != '\0'; j++)
-            {
+            for (int j = 0; creditos[j] != '\0'; j++) {
                 resultado[posicionResultado] = creditos[j];
                 posicionResultado++;
             }
-                resultado[posicionResultado] = ' ';
-                posicionResultado++;
+            resultado[posicionResultado] = ' ';
+            posicionResultado++;
 
-            for(int j = 0; horas[j] != '\0'; j++)
-            {
+            for (int j = 0; horas[j] != '\0'; j++) {
                 resultado[posicionResultado] = horas[j];
                 posicionResultado++;
             }
-                resultado[posicionResultado] = '\n';
-                posicionResultado++;
-
+            resultado[posicionResultado] = '\n';
+            posicionResultado++;
 
             resultado[posicionResultado] = '\0';
-            // agregamos el caracter nulo al final del arreglo resultado
+
+            // Mostrar la información
+            cout << "Codigo: " << codigo << endl;
+            cout << "Nombre: " << nombre << endl;
+            cout << "Creditos: " << creditos << endl;
+            cout << "Horas: " << horas << endl;
 
             delete[] codigo;
             delete[] nombre;
             delete[] creditos;
             delete[] horas;
-
-            // mostrar la informaci0n
-            cout << "Codigo: " << codigo << endl;
-            cout << "Nombre: " << nombre << endl;
-            cout << "Creditos: " << creditos << endl;
-            cout << "Horas: " << horas << endl;
-            return resultado;
-
-
         }
-
-
-
-
     }
-    delete[] codigo;
-    delete[] nombre;
-    delete[] creditos;
-    delete[] horas;
-    return nullptr;
- // si no encontramos el código, retornar nullptr
-}
 
+    if (!encontrado) {
+        cout << "El codigo no se encontro" << endl;
+    }
+
+    return resultado;
+}
 
 bool es_igual(const char*c1, const char*c2){
     int i=0;
@@ -169,5 +154,7 @@ bool es_igual(const char*c1, const char*c2){
     }
     return true;
 }
+
+
 
 
